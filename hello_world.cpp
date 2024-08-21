9ab2617639142073a257670040a9c1cbab55f84d
arpitparekh <arpitparekh9@gmail.com>
2024-08-21 11:14:44 +0530

    hello world

---
+++ hello_world.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+typedef int i;
+
+using namespace std;
+int main()
+{
+
+    /*
+
+    C++ is one of the most used and popular programming languages.
+
+    C++ is used in making operating systems, embedded systems, and Graphical User Interfaces.
+
+    It is an object-oriented programming language that implements all the OOPs concepts 
+    such as Abstraction, Encapsulation, and Inheritance, which gives a clear structure to 
+    programs and allows code to be reused, lowering development costs and providing security.
+
+    It is portable and can be used to create applications that can be adapted to multiple 
+    platforms.
+
+    C++ is easy to learn so that you can choose it as your first programming language.
+
+    It makes programming easy for programmers to switch to C++ because its syntax is similar to C,
+    Java, and C#.
+
+    */
+
+    cout << "Hello World\n";
+    cout << "Hello Faisal\n";
+    cout << "Welcome to C++ Batch\n";
+
+    i a;
+    cin >> a;
+    cout << a << "\n"; // forget the format specifiers
+
+    // float b;
+    // cin >> b;
+    // cout << b;
+
+    int u = 34;
+
+    cout << "Value of x is " << u << " and " << a << endl;
+
+    int x, y;
+    cin >> x >> y;
+    cout << x << " " << y << endl;
+
+    // oopc
+    // object oriented programming concepts
+    // class
+    // object
+
+    // polymorphism
+
+    // encapsulation
+
+    // abstraction
+
+    // inheritance
+
+    // constructor
+    // copy constrcutor
+
+    // method overloading
+    // method overriding
+    // operator overloading
+
+    // multi threading
+    // exaception handling
+    // file handling
+
+    return 0;
+}
