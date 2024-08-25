4fa93dd927a821a08b1a394570cffc8c1900e066
arpitparekh <arpitparekh9@gmail.com>
2024-08-25 15:50:22 +0530

    class object

--- oopc/assign_value_using_function.cpp
+++ oopc/assign_value_using_function.cpp
@@ -8,22 +8,35 @@ class Circle{
   public:
 
   float radius = 0;
-
   void assignValues(float r){
     radius = r;
   }
 
+  void findPerimeter(){
+    cout << "Perimeter: " << 2 * 3.14 * radius << endl;
+  }
+
+  void findArea(){
+    cout << "Area: " << 3.14 * radius * radius << endl;
+  }
+
   void printInfo(){
     cout << "Radius: " << radius << endl;
-    }
+  }
+
 };
 
 int main(){
+
   Circle c;
 
   c.assignValues(10);
-
   c.printInfo();
+  c.findPerimeter();
+  c.findArea();
+
+
 
   return 0;
+
 }
