709dd9e1f37b2315c71144491d93d77fe231ab0c
arpitparekh <arpitparekh9@gmail.com>
2024-09-02 12:20:09 +0530

    thigns to do and done

---
+++ things_in_c++.cpp
@@ -0,0 +1,56 @@
+// make a list that we have done on cpp from now
+
+// general
+// iosteam
+// namespace
+
+// variables
+// datatypes
+// enum
+// ternary operator
+// reference datatype
+
+// oopcs
+// classes
+// objects
+// constructor
+// descturctor
+
+// polymorphism
+// function overloading
+// function overriding
+
+// inheritance
+// encapsulation  // access modifiers
+
+// copy constructor
+// static data members
+
+// remaining  //////////////////////////
+
+// virtual functions
+// storage class
+// friend function
+// friend class
+
+// static class
+// static block
+
+// operator overloading
+// abstract class
+// interface
+// exception handling
+// file hanlding
+// template  // generics
+
+// namespace
+// multi threading
+
+
+// STL
+// STL containers
+// STL iterators
+
+
+
+
